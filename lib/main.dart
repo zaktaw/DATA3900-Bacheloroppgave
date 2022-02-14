9f931deb59e960984e030d0a73828f6435aab6ee
Peter <peter.stjernsund@gmail.com>
2022-02-14 13:49:10 +0100

    Bottom btn

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'activity_bottombar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar
+        appBar: AppBar(
           title: Text('Hei gutter!'),
         ),
+        bottomNavigationBar: ActivityBottombar(),
       ),
     );
   }
