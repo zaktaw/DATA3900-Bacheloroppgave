25a9f526055ad716318a058c87940685c36962c9
Peter <peter.stjernsund@gmail.com>
2022-02-14 10:28:30 +0100

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('TTT'),
+          title: Text('Hei gutter!'),
         ),
       ),
     );
