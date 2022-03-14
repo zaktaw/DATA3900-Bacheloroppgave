e20cd9338cf5c0b581356f3fd6413ba6c03f5f70
matsovevada <55790581+matsovevada@users.noreply.github.com>
2022-03-14 13:45:53 +0100

    Update settings.dart

--- lib/settings_page/settings.dart
+++ lib/settings_page/settings.dart
@@ -1,9 +1,11 @@
 import 'package:bacheloroppgave/settings_page/settings_help_topbar.dart';
 import 'package:flutter/material.dart';
 
+const String settings = 'Innstillinger';
+const String settings_text = 'Innstillinger placeholder';
+
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
-
 
   @override
   _SettingsState createState() => _SettingsState();
@@ -13,10 +15,7 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SettingsHelpTopBar("Innstillinger", '/', null),
-      body: Container(
-        child: const Text("Settings placeholder")
-      ) 
-    );
+        appBar: SettingsHelpTopBar(settings, '/', null),
+        body: Container(child: const Text(settings_text)));
   }
 }
