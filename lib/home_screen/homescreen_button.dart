72e06b2965a89bcd8c92396994d574a6f11f0537
matsovevada <55790581+matsovevada@users.noreply.github.com>
2022-03-21 11:56:46 +0100

    Update homescreen_button.dart

--- lib/home_screen/homescreen_button.dart
+++ lib/home_screen/homescreen_button.dart
@@ -1,15 +1,17 @@
 import 'package:bacheloroppgave/models/TttEntries.dart';
+import 'package:bacheloroppgave/resources/app_theme.dart';
 import 'package:bacheloroppgave/zone_page/zone_card.dart';
 import 'package:flutter/material.dart';
 
+const double buttonWidth = 175;
+
 class HomeScreenButton extends StatefulWidget {
-  HomeScreenButton(
-      {Key? key,
-      required this.btnName,
-      required this.route,
-      required this.args,
-      })
-      : super(key: key);
+  HomeScreenButton({
+    Key? key,
+    required this.btnName,
+    required this.route,
+    required this.args,
+  }) : super(key: key);
 
   final String btnName;
   final String route;
@@ -22,10 +24,21 @@ class HomeScreenButton extends StatefulWidget {
 class _HomeScreenButtonState extends State<HomeScreenButton> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        child: Text(widget.btnName),
-        onPressed: () {
-          Navigator.of(context).pushNamed(widget.route, arguments: widget.args);
-        });
+    return Container(
+        margin: const EdgeInsets.only(top: 10.0),
+        child: ElevatedButton(
+            child: Text(
+              widget.btnName,
+              style: const TextStyle(color: TEXT_COLOR_BLACK),
+            ),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(SECONDARY_COLOR),
+                padding: MaterialStateProperty.all(const EdgeInsets.all(8)),
+                fixedSize: MaterialStateProperty.all(
+                    const Size.fromWidth(buttonWidth))),
+            onPressed: () {
+              Navigator.of(context)
+                  .pushNamed(widget.route, arguments: widget.args);
+            }));
   }
 }
