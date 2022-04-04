ecf97b212c8987198292c030796925fee3c1178e
Peter <peter.stjernsund@gmail.com>
2022-04-04 14:18:13 +0200

    div assetts fix

--- lib/help_page/about_app_pageviews/fourth_pageview.dart
+++ lib/help_page/about_app_pageviews/fourth_pageview.dart
@@ -11,7 +11,7 @@ class FourthPageview extends StatelessWidget {
         children: [
           Text(FOURTH_PAGEVIEW_STRING_TITLE, textAlign: TextAlign.center, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Text(FOURTH_PAGEVIEW_STRING), 
-          Container(child: Image.asset("images/mainmenu_example.gif"), decoration: BoxDecoration(border: Border.all(color: Colors.black)), height: MediaQuery.of(context).size.height * GIF_PAGEVIEW_MARGIN_FACTOR_HEIGHT, width: MediaQuery.of(context).size.height * GIF_PAGEVIEW_MARGIN_FACTOR_WIDTH),
+          Container(child: Image.asset("assets/images/mainmenu_example.gif"), decoration: BoxDecoration(border: Border.all(color: Colors.black)), height: MediaQuery.of(context).size.height * GIF_PAGEVIEW_MARGIN_FACTOR_HEIGHT, width: MediaQuery.of(context).size.height * GIF_PAGEVIEW_MARGIN_FACTOR_WIDTH),
       ],
     
     );
--- lib/help_page/about_app_pageviews/third_pageview.dart
+++ lib/help_page/about_app_pageviews/third_pageview.dart
@@ -11,7 +11,7 @@ class ThirdPageview extends StatelessWidget {
       children: [
       Text(THIRD_PAGEVIEW_STRING_TITLE, textAlign: TextAlign.center, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
       Text(THIRD_PAGEVIEW_STRING), 
-      Container(child: Image.asset("images/mainmenu_example.gif"), decoration: BoxDecoration(border: Border.all(color: Colors.black)), height: MediaQuery.of(context).size.height * GIF_PAGEVIEW_MARGIN_FACTOR_HEIGHT, width: MediaQuery.of(context).size.height * GIF_PAGEVIEW_MARGIN_FACTOR_WIDTH),
+      Container(child: Image.asset("assets/images/mainmenu_example.gif"), decoration: BoxDecoration(border: Border.all(color: Colors.black)), height: MediaQuery.of(context).size.height * GIF_PAGEVIEW_MARGIN_FACTOR_HEIGHT, width: MediaQuery.of(context).size.height * GIF_PAGEVIEW_MARGIN_FACTOR_WIDTH),
       ],
   
     );
