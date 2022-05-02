adaf81c30c6b9f89adde8da19f1fccb0dbd38c2d
zaktaw <zaktaw@gmail.com>
2022-05-02 14:17:25 +0200

    Add comments on local_storage_hive

--- lib/local_storage_hive/TttEntriesBox.dart
+++ lib/local_storage_hive/TttEntriesBox.dart
@@ -1,6 +1,8 @@
 import 'package:hive/hive.dart';
 import 'package:bacheloroppgave/models/TttEntries.dart';
 
+// Model for Hive ttt entries box
+// used for storing ttt entries in local storage on phone
 class TttEntriesBox {
   static Box<TttEntries> getTttEntries() =>
       Hive.box<TttEntries>('tttEntries');
--- lib/local_storage_hive/TttProjectInfoBox.dart
+++ lib/local_storage_hive/TttProjectInfoBox.dart
@@ -2,6 +2,8 @@ import 'package:bacheloroppgave/models/TttEntries.dart';
 import 'package:bacheloroppgave/models/TttProjectInfo.dart';
 import 'package:hive/hive.dart';
 
+// Model for Hive ttt project info box
+// used for storing project info in local storage on phone
 class TttProjectInfoBox {
   static Box<TttProjectInfo> getTttProjectInfo() =>
       Hive.box<TttProjectInfo>('tttProjectInfo');
